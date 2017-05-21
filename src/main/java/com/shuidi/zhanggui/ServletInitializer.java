cce41c1bf038c7bebe41b1893c640c35c36c78a5
Houyefeng <46106962@qq.com>
2017-05-21 10:36:19 +0800

    reset author

---
+++ src/main/java/com/shuidi/zhanggui/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.shuidi.zhanggui;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ZhangguiApplication.class);
+	}
+
+}
---
+++ src/main/java/com/shuidi/zhanggui/ZhangguiApplication.java
@@ -0,0 +1,12 @@
+package com.shuidi.zhanggui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhangguiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZhangguiApplication.class, args);
+	}
+}
