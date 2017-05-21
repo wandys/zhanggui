23b4c6e42005f0133991b662d28ea4d59575b6ae
Yefei <3506391626@qq.com>
2017-05-21 17:08:14 +0800

    增加Logger

--- src/main/java/com/shuidi/zhanggui/ServletInitializer.java
+++ src/main/java/com/shuidi/zhanggui/ServletInitializer.java
@@ -1,9 +1,12 @@
 package com.shuidi.zhanggui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
+	private static final Logger log = LoggerFactory.getLogger(ServletInitializer.class);
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
